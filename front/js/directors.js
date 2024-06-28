a9312e3ad002ce8858521aa5aeab31ae7f12d6dc
Hernan Matias Ortiz <hernanortiz18@gmail.com>
2024-06-28 02:58:40 -0300

    fix getAllDirectors()

--- front/js/directors.js
+++ front/js/directors.js
@@ -1,12 +1,61 @@
-import utils from './utils';
 
-const {Directors} = utils;
 
-const listDirectors = async() => {
-    try{
-        const arrayDirector = await Directors.getAllDirectors();
-        console.log(arrayDirector)
-        const inputDirectors = document.getElementById('select_directores')
+
+    
+    const postOptions = (data) => {return {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)}}
+    
+    // deleteOptions : {method: 'DELETE', headers: {'Content-Type':'application/json'}, body: JSON.stringify({id})},
+    
+    // putOptions : (data) => { return {method: 'PUT', headers: {'Content-Type':'application/json'}, body: JSON.stringify(data)}},
+
+    const getAllDirectors = () => {
+        fetch('http://localhost/movie-php-src/controlador/cargar_directores.php', {method: 'GET'})
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            listDirectors(response)
+        })
+        .catch((err)=>console.error(err))
+    }
+
+    const addDirector= (data) => {
+        fetch('http://localhost/movie-php-src/controlador/alta/alta_directores.php', postOptions(data))
+        .then((response) => {
+            if (response.ok) return response
+            else (console.log('Error conection'))
+        })
+        .then(response=>{
+            alert('Película cargada con éxito')
+            console.log(response)
+        })
+        .catch((err)=>console.error(err))
+    }
+    // deleteDirector: (id) => {
+    //     fetch('', Directors.deleteOptions)
+    //     .then(response=>{
+    //         if (response.status(204)) {
+    //             console.log('Película eliminada con éxito')
+    //             alert('')
+    //         }
+    //     })
+    // },
+    // updateDirector: (data) => {
+    //     fetch('', Directors.putOptions)
+    //     .then(response => {
+    //         if (response.status===200) return response
+    //         else (console.log('Error conection'))
+    //     })
+    //     .then(response => {
+    //         alert('Película actualizada con éxito')
+    //         console.log(response)
+    //     })
+    // }
+
+
+
+const listDirectors = (arrayDirector) => {
+    try{        
+        const inputDirectors = document.getElementById('select_directores');
         arrayDirector.forEach((director)=>{
             optionDirector = document.createElement('option')
             optionDirector.value = director.id_director;
@@ -20,5 +69,5 @@ const listDirectors = async() => {
 }
 
 document.addEventListener('DOMContentLoaded',()=>{
-    listDirectors();
+    getAllDirectors();
 })
