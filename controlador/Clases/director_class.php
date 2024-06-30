158cdc70f7982b8fb8284b578e6cb472b55abfc6
Marina <marina@estudioelangel.com.ar>
2024-06-30 13:01:47 -0300

    Modif.classes-altas-para funcionar como Api-recibir los datos desde un javaScrip y no desde un form-Modif. link del logo en administrador para ir al inicio.html-

--- controlador/Clases/director_class.php
+++ controlador/Clases/director_class.php
@@ -4,13 +4,33 @@ class Director {
     public $apellido;
     public $fechaNacimiento;
     public $nacionalidad;
-
     public function __construct($nombre, $apellido, $fechaNacimiento, $nacionalidad) {
         $this->nombre = $nombre;
         $this->apellido = $apellido;
         $this->fechaNacimiento = $fechaNacimiento;
         $this->nacionalidad = $nacionalidad;
+    } 
+    
+    function insertarDirector($director, $conn) {
+
+        if (!empty($director->nombre) && !empty($director->apellido) && !empty($director->fechaNacimiento) && !empty($director->nacionalidad)) {
+               
+            // Preparar la sentencia
+            $stmt = $conn->prepare("INSERT INTO directores (d_nombre, d_apellido, d_fecha_nacimiento, d_nacionalidad) VALUES (?, ?, ?, ?)");
+            $stmt->bind_param("ssss", $director->nombre, $director->apellido, $director->fechaNacimiento, $director->nacionalidad);
+            // Ejecutar la sentencia
+            $stmt->execute();
+                echo "Nuevo Director ingresado con exito";
+                respuestaJson(201, 'Director insertado correctamente');
+                //cirre del sql y la conexión
+                $stmt->close();
+                $conn->close();
+        } else {
+            echo "El Director no puedo ser ingresado con exito";
+        
     }
+}
+
 }
 ?>
 
--- controlador/cargar_directores.php
+++ controlador/cargar_directores.php
@@ -2,10 +2,8 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); 
 include './ingresosDB/conn.php';
-
 // Consulta SQL para obtener los directores
 $sql = "SELECT id_director, d_nombre, d_apellido FROM directores";
 $resultado = $conn->query($sql);
