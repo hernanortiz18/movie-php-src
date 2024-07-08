e7442be314c89e38ab2709083c3669e02f9c91c0
Hernan Matias Ortiz <hernanortiz18@gmail.com>
2024-07-08 14:30:36 -0300

    module movies.js

--- front/js/directors.js
+++ front/js/directors.js
@@ -48,7 +48,7 @@ const listDirectors = (arrayDirector) => {
     try{        
         const inputDirectors = document.getElementById('select_directores');
         arrayDirector.forEach((director)=>{
-            optionDirector = document.createElement('option')
+            let optionDirector = document.createElement('option')
             optionDirector.value = director.id_director;
             optionDirector.textContent = `${director.d_nombre} ${director.d_apellido}`
             inputDirectors.appendChild(optionDirector)
